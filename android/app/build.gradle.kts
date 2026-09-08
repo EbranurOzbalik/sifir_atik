f81f4d33d8520d6b463c265c78cb3dfcbcf4203f
Ebranur Özbalık <98876341+EbranurOzbalik@users.noreply.github.com>
2026-09-08 21:30:23 +0300

    Son demo düzenlemelerini yaptım

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,6 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.sifir_atik"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
@@ -31,7 +30,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
